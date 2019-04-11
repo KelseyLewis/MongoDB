72a63d8386478e8964aed5c913111622de88677c
Kelsey Lewis <kelseygueldalewis@gmail.com>
2019-04-11 14:19:03 -0700

    Passport

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -11,24 +11,7 @@ router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
-router.post('/signup', (req, res, next) => {
-  User.register(new User({username: req.body.username}), 
-    req.body.password, (err, user) => {
-    if(err) {
-      console.log(err);
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({err: err});
-    }
-    else {
-      passport.authenticate('local')(req, res, () => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({success: true, status: 'Registration Successful!'});
-      });
-    }
-  });
-});
+  
 
 //first call passport, 
 router.post('/login', passport.authenticate('local'), (req,res) => {
